f30d2f6551340461e8617d092fe85af24e9342c7
Helga Ish <belfegor.nomnom@gmail.com>
2023-06-14 17:32:51 +0800

    fix: rmv validator import

--- models/card.js
+++ models/card.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { validator } = require('express');
+// const { validator } = require('express');
 
 const cardSchema = new mongoose.Schema({
   name: {
--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { validator } = require('express');
+// const { validator } = require('express');
 
 const userSchema = new mongoose.Schema({
   name: {
