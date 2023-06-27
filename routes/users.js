b9233b625df69280ab3cd67017017a637bad2e5b
Helga Ish <belfegor.nomnom@gmail.com>
2023-06-27 13:48:04 +0800

    feat: change path for getUserById + rmv createUser

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,12 @@ const router = require('express').Router();
 const {
   getUsers,
   getUserById,
-  createUser,
   updateProfile,
   updateAvatar,
 } = require('../controllers/users');
 
 router.get('/users', getUsers);
-router.get('/users/:userId', getUserById);
-router.post('/users', createUser);
+router.get('/users/me', getUserById);
 
 router.patch('/users/me', updateProfile);
 router.patch('/users/me/avatar', updateAvatar);
