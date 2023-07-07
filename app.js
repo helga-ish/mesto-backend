cca35f10c505cf84d78a259d3a634f9560abe084
Helga Ish <belfegor.nomnom@gmail.com>
2023-07-07 16:10:49 +0800

    feat: add loggers

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const {
   createUser,
 } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const processErrors = require('./middlewares/processErrors');
 const NotFoundError = require('./components/NotFoundError');
 
@@ -24,6 +25,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
+
+app.use(requestLogger);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -46,6 +49,8 @@ app.use('/', auth, require('./routes/users'));
 app.use('/', auth, require('./routes/cards'));
 
 app.use('*', (req, res, next) => next(new NotFoundError()));
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(processErrors);
